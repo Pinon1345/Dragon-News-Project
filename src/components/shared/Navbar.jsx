89fff2dcea2ba5b350ac33977880f132ccb3635d
Pinon1345 <pinonfurkan1@gmail.com>
2026-06-09 03:15:32 +0600

    implement function in login/logout button in Navbar and redirect path in Registration and Login section

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import userAvatar from "@/assets/user.png";
 import NavLink from './NavLink';
+import { authClient } from '@/lib/auth-client';
 
 const Navbar = () => {
+
+    const { data: session, isPending } = authClient.useSession()
+    const user = session?.user;
+
+
+    console.log("Session", user, isPending);
+
     return (
         <div className='container w-11/12 mx-auto flex justify-between items-center gap-4 py-4'>
             <div></div>
@@ -13,21 +23,35 @@ const Navbar = () => {
                 <li><NavLink href="/about">About</NavLink></li>
                 <li><NavLink href="/career">Career</NavLink></li>
             </ul>
-            <div className='flex md:flex-row flex-col items-center gap-3'>
-                <Image
-                    src={userAvatar}
-                    alt='User Avatar'
-                    width={40}
-                    height={40}
-                >
-                </Image>
-                <button
-                    className='btn btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-black text-white px-5 py-2 rounded-md font-semibold text-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl hover:bg-purple-600'>
-                    <Link href={"/login"}>
-                        Login
-                    </Link>
-                </button>
-            </div>
+
+            {
+                isPending ? (<span className="loading loading-spinner text-info"></span>) :
+                    user ?
+                        (<div className='flex md:flex-row flex-col items-center gap-3'>
+                            <h2 className='font-bold text-lg'>Hello, <span className='font-bold text-xl text-blue-600'>{user.name}</span></h2>
+                            <Image
+                                className='w-13 h-13 rounded-full object-cover border-2 border-gray-300'
+                                src={user.image || userAvatar}
+                                alt='User Avatar'
+                                width={40}
+                                height={40}
+                            >
+                            </Image>
+                            <button
+                                className='btn btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-black text-white px-5 py-2 rounded-md font-semibold text-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl hover:bg-purple-600'
+                            >
+                                Log out
+                            </button>
+                        </div>
+                        ) : (
+                            <button
+                                className='btn btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-black text-white px-5 py-2 rounded-md font-semibold text-lg transition-all duration-300 hover:-translate-y-1 hover:shadow-xl hover:bg-purple-600'>
+                                <Link href={"/login"}>
+                                    Login
+                                </Link>
+                            </button>
+                        )
+            }
         </div>
     );
 };
